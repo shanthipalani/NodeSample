8a399ac6c8ff172f5dfea4d0baff4014bda46d12
shpalani <shpalani@paypal.com>
2018-11-01 19:25:53 +0530

    db connection

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var db = require('./queries');
 */
 
 router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/puppies/:id', db.getSinglePuppy);
+router.get('/api/puppies/:ids', db.getSinglePuppy);
 router.post('/api/puppies', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
 router.delete('/api/puppies/:id', db.removePuppy);
--- routes/queries.js
+++ routes/queries.js
@@ -26,6 +26,22 @@ function getAllPuppies(req, res, next) {
 }
 
 function getSinglePuppy(req, res, next) {
+    var pupID = parseInt(req.params.ids);
+    db.one('select * from pups where id = $1', pupID)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ONE puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function searchPuppy(req, res, next) {
     var pupID = parseInt(req.params.id);
     db.one('select * from pups where id = $1', pupID)
         .then(function (data) {
